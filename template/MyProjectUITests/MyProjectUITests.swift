c5107fa6da40124db5ec945512eb2f4d454f50c7
Cameron Cooke <web@cameroncooke.com>
2025-06-13 15:04:49 +0100

    Fix UI test target

--- template/MyProjectUITests/MyProjectUITests.swift
+++ template/MyProjectUITests/MyProjectUITests.swift
@@ -22,13 +22,6 @@ final class MyProjectUITests: XCTestCase {
         app.launch()
 
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    @MainActor
-    func testLaunchPerformance() throws {
-        // This measures how long it takes to launch your application.
-        measure(metrics: [XCTApplicationLaunchMetric()]) {
-            XCUIApplication().launch()
-        }
+        XCTAssertTrue(true)
     }
 }
